3ff02eca0ee2ebd96f5ea95319241c38193b5678
Kyle Wolfson <wolfsonk@gmail.com>
2016-04-22 10:39:43 -0700

    pass integration tests for each page

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,50 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Word Puzzle");
+  }
+  @Test
+  public void puzzleCreationTest() {
+    goTo("http://localhost:4567/");
+    fill("#userString").with("Hello");
+    submit(".btn");
+    assertThat(pageSource()).contains("H-ll-");
+  }
+  @Test
+  public void GuessTestFailure() {
+    goTo("http://localhost:4567/");
+    fill("#userString").with("Hello");
+    submit(".btn");
+    fill("#guessString").with("This is a terrible guess");
+    submit(".btn");
+    assertThat(pageSource()).contains("Sorry, that is not the correct answer.");
+  }
+  @Test
+  public void GuessSuccess() {
+    goTo("http://localhost:4567/");
+    fill("#userString").with("Hello");
+    submit(".btn");
+    fill("#guessString").with("Hello");
+    submit(".btn");
+    assertThat(pageSource()).contains("Congratulations, you solved the puzzle!");
+  }
+}
